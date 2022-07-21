09044acf7ae719bcfabaabf9c24acc7be1366514
Nza <60954472+Nicholas-Zarate@users.noreply.github.com>
2022-07-21 10:05:41 -0500

    Update server.js
    Added Access Header to GET "/"

--- server2/server.js
+++ server2/server.js
@@ -1,26 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require('cors')
-const app = express()
-app.use(cors())
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(cors());
 
-mongoose.connect(process.env.MDB_URL, { useNewUrlParser: true});
-const db = mongoose.connection
+mongoose.connect(process.env.MDB_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
 
 //connection message
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to db"));
 
-console.log("hello")
-
-
+console.log("hello");
 
 //express set up
-app.use(express.json())
+app.use(express.json());
 
-const itemsRouter = require('./routes/items');
+const itemsRouter = require("./routes/items");
 
-app.use('/grocery_store', itemsRouter);
-app.listen(5000,() => console.log("Server started"));
+app.use("/grocery_store", itemsRouter);
+app.listen(5000, () => console.log("Server started"));
